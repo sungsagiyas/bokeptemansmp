4de6371cc42846842457c14c2884a5b7ad4c39aa
sungsagiyas <sungsagiyas@gmail.com>
2025-07-15 14:33:47 +0700

    Delete src directory

--- src/utils/site.ts
+++
@@ -1,4 +0,0 @@
-export const nama = 'Bokep Teman Smp';
-export const desk = 'Video Bokep Indo Bocil Ngentot Jilbab Smp Mama Viral Sma Cindo Tante China';
-export const url = 'https://bokeptemansmp.pages.dev';
-export const terbit = '2025-01-27T00:01:03Z';
