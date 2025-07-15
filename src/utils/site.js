acc28a831507d2b9a9b341c6a8c90f26d594e111
sungsagiyas <sungsagiyas@gmail.com>
2025-07-15 14:36:13 +0700

    Add files via upload

---
+++ src/utils/site.js
@@ -0,0 +1,9 @@
+export const nama = 'Bokep Teman Smp';
+export const desk = 'Video Bokep Indo Bocil Ngentot Jilbab Smp Mama Viral Sma Cindo Tante China';
+export const url = 'https://bokeptemansmp.pages.dev';
+export const terbit = '2025-01-27T00:01:03Z';
+
+export const google = 'n88cnhFb4iG2kl16bHF0L7IsOnbYklE0JCu8DHT-x5s';
+export const yandex = '59d5d3ca016f1a6e'
+
+export const index = '3a3a80fb-33cc-42d5-a2d4-d6a662b8fd73';
